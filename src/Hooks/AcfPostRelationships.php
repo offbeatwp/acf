4fa2273509ef9108ca6cc949b39b9e5e9e973eb5
maikelRAOW <80957439+maikelRAOW@users.noreply.github.com>
2022-01-18 14:48:50 +0100

    Revert "Add a static updateRelation method."

--- src/Hooks/AcfPostRelationships.php
+++ src/Hooks/AcfPostRelationships.php
@@ -1,43 +1,33 @@
 <?php
 namespace OffbeatWP\Acf\Hooks;
 
-use OffbeatWP\Exceptions\OffbeatModelNotFoundException;
+use Exception;
 use OffbeatWP\Hooks\AbstractFilter;
 
 class AcfPostRelationships extends AbstractFilter {
     /**
-     * @param mixed $value The new value.
-     * @param int|string $postId The post ID where the value is saved.
-     * @param array $field The field array containing all settings.
-     * @param mixed $_value The original value before modification.
+     * @throws Exception
+     * @param mixed $value
+     * @param int|string $postId
+     * @param array $field
+     * @param mixed $_value
      * @return mixed
      */
     public function filter($value, $postId, $field, $_value) {
-        self::updateRelation($value, $postId, $field['name']);
-        return $value;
-    }
-
-    /**
-     * @param mixed $value The new value.
-     * @param int|numeric-string $postId The post ID to update.
-     * @param int|string $metaKey The key of the meta-field that the relation is attached to.
-     * @return void
-     */
-    public static function updateRelation($value, $postId, $metaKey) {
         if (!is_numeric($postId)) {
-            return;
+            return $value;
         }
 
         $post = offbeat('post')->get($postId);
 
         if (!$post) {
-            throw new OffbeatModelNotFoundException('Post not found with id: ' . $postId);
+            throw new Exception('Post not found');
         }
 
-        $method = $post->getMethodByRelationKey($metaKey);
+        $method = $post->getMethodByRelationKey($field['name']);
 
-        if ($method === null || !is_callable([$post, $method])) {
-            return;
+        if (is_null($method) || !is_callable([$post, $method])) {
+            return $value;
         }
 
         if (!$value) {
@@ -47,7 +37,7 @@ class AcfPostRelationships extends AbstractFilter {
                 $post->$method()->dissociateAll();
             }
 
-            return;
+            return $value;
         }
 
         $relationships = $value;
@@ -63,5 +53,7 @@ class AcfPostRelationships extends AbstractFilter {
         } else {
             $post->$method()->associate($relationships, false);
         }
+        
+        return $value;
     }
 }
