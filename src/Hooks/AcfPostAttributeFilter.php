159a9075bc7189577348958b2677ee4f55122d6f
Maikel Haarmans <maikel@raow.nl>
2022-06-02 13:28:38 +0200

    Fix incorrect id filter

--- src/Hooks/AcfPostAttributeFilter.php
+++ src/Hooks/AcfPostAttributeFilter.php
@@ -1,10 +1,11 @@
 <?php
 namespace OffbeatWP\Acf\Hooks;
 
+use OffbeatWP\Content\Post\PostModel;
 use OffbeatWP\Hooks\AbstractFilter;
 
 class AcfPostAttributeFilter extends AbstractFilter {
-    public function filter ($value, string $name, $model) {
-        return get_field($name, $model->id) ?: $value;
+    public function filter($value, string $name, PostModel $model) {
+        return get_field($name, $model->getId()) ?: $value;
     }
 }
